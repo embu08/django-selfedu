a2b103cda861975ac1020c6140ce2f10011c7144
embu08 <a800x800@gmail.com>
2022-08-09 11:40:26 +0300

    finished 4th video, created model Cats, added MEDIA_ROOT and MEDIA_URL to settings, migrations has been done

--- django_selfedu/django_selfedu/urls.py
+++ django_selfedu/django_selfedu/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
 from cats.views import *
+from django_selfedu import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cats.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = page_not_found
