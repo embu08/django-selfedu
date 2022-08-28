6bdb8e38e3d3d107525901540bfd4cd3f156640f
embu08 <a800x800@gmail.com>
2022-08-28 12:49:33 +0300

    last lesson, some changes in django admin template

--- django_selfedu/cats/admin.py
+++ django_selfedu/cats/admin.py
@@ -1,15 +1,24 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import *
 
 
 class BreedsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'photo', 'is_published', 'category')
+    list_display = ('id', 'title', 'time_create', 'get_html_photo', 'is_published', 'category')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create', 'category')
     prepopulated_fields = {'slug': ('title',)}
+    fields = ('title', 'slug', 'category', 'get_html_photo', 'photo', 'is_published', 'time_create', 'time_update', )
+    readonly_fields = ('time_create', 'time_update', 'get_html_photo')
+
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.url}' width=150 alt='{object.title}'")
+
+    get_html_photo.short_description = 'Photo'
 
 
 class CategoryAdmin(admin.ModelAdmin):
